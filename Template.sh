ce4f80fc66b055d674fc8855d59dab31e60cd038
dericush <hornerdericus@gmail.com>
2023-02-08 23:28:11 +0000

    Assignment completed

---
+++ Template.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Dericus Horner
+# Date of latest revision:
+# Purpose: Print a string to the terminal
+
+# Main 
+
+
+# End
---
+++ ops-challenge03.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Dericus Horner
+# Date of latest revision:02/08/2023
+# Purpose: Writing script that prints the login history of user
+
+# Main 
+var1=$(last)
+func1(){
+echo "$var1"
+}
+func1
+
+
+
+# End
