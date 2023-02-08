f38d36b13190c816c14dbba331429af81b40b1dc
dericush <hornerdericus@gmail.com>
2023-02-08 02:43:55 +0000

    challenge 02 Completed

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Script: Ops 201 Class 02 Ops Challenge Solution
+# Author:
+# Date of latest revision:
+# Purpose: Print a string to the terminal
+
+# Main
+
+var="I am working perfectly"
+echo $var
+# End
