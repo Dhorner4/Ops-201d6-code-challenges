e24755ccef894a148a69d4bccf9150dd0228d504
dericush <hornerdericus@gmail.com>
2023-02-21 02:01:57 +0000

    Code Challenge 11 Complete

---
+++ ops-challenge11.ps1
@@ -0,0 +1,18 @@
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+# Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 
+
+# Enable Remote management
+Enable-PSRemoting -Force
+
+# Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Disable-WindowsOptionalFeature -Online -FeatureName smb1protcol
+
